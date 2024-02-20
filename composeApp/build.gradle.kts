8d6116ea181abacd0eac86376d64195bd24ece78
anwar <anwar_pasaribu@live.com>
2024-02-20 11:38:29 +0700

    up version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -87,8 +87,8 @@ android {
         applicationId = "com.unwur.etong"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
     buildFeatures {
         compose = true
