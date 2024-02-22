7426f567894cda4663dd80c62eb42d30624cbd40
anwar <anwar_pasaribu@live.com>
2024-02-22 12:44:56 +0700

    Code cleanup

--- composeApp/src/desktopMain/kotlin/ui/theme/PlatformTheme.desktop.kt
+++ composeApp/src/desktopMain/kotlin/ui/theme/PlatformTheme.desktop.kt
@@ -2,8 +2,6 @@ package ui.theme
 
 import androidx.compose.material3.ColorScheme
 import androidx.compose.runtime.Composable
-import ui.theme.AppDarkColors
-import ui.theme.AppLightColors
 
 @Composable
 internal actual fun colorScheme(
