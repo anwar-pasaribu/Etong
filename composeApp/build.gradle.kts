958f42f092963d4b3a580dc74d5f29a74aa1acf8
anwar <anwar_pasaribu@live.com>
2024-03-01 16:50:09 +0700

    update app version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -107,8 +107,8 @@ android {
         applicationId = "com.unwur.etong"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.2.1"
     }
     buildFeatures {
         compose = true
