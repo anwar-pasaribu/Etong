b4daa5ba88aea33cbc752d3760f0006962b1d60d
anwar <anwar_pasaribu@live.com>
2024-03-03 08:32:49 +0700

    test config #10 - caching strategies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -124,8 +124,8 @@ android {
         applicationId = "com.unwur.etong"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 124
-        versionName = "1.2.4"
+        versionCode = 125
+        versionName = "1.2.5"
 
         ndk {
             abiFilters.add("armeabi-v7a")
