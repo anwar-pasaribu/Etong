09d8a4b1cf85e2f69b7f1dd18cdadb06f9df35ae
anwar <anwar_pasaribu@live.com>
2024-03-08 21:20:11 +0700

    cleanup

--- composeApp/src/commonMain/kotlin/utils/ImagePicker.kt
+++ composeApp/src/commonMain/kotlin/utils/ImagePicker.kt
@@ -1,16 +1,6 @@
 package utils
 
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.ImageBitmap
-
-/**
- * Picks image file from platform's system and gives file data in [onResult]
- *
- * @param onResult Will be invoked when file is picked. Will be invoked with `null` when file is
- * not picked
- */
-@Composable
-expect fun ImagePicker(onResult: (ByteArray?) -> Unit)
 
 /**
  * Creates [ImageBitmap] from this [ByteArray]
